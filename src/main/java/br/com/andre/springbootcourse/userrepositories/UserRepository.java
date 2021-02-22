cc9e8f4063bac542026db056168b7cf3786c884e
Andre Santana <andrelusantana@hotmail.com>
2021-02-22 00:13:12 -0300

    Service layer, component registration

--- src/main/java/br/com/andre/springbootcourse/userrepositories/UserRepository.java
+++ src/main/java/br/com/andre/springbootcourse/userrepositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.andre.springbootcourse.userrepositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.andre.springbootcourse.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
