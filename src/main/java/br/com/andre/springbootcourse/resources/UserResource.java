6677f89cbd5ae5e0ef57b40896c43e18ed612b16
Andre Santana <andrelusantana@hotmail.com>
2021-02-24 20:05:11 -0300

    User insert

--- src/main/java/br/com/andre/springbootcourse/resources/UserResource.java
+++ src/main/java/br/com/andre/springbootcourse/resources/UserResource.java
@@ -1,14 +1,17 @@
 package br.com.andre.springbootcourse.resources;
 
+import java.net.URI;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.andre.springbootcourse.entities.User;
 import br.com.andre.springbootcourse.services.UserService;
@@ -32,4 +35,12 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 		}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user){ //@RequestBody -> fará a desserialização do Json em OBJ User 
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		// objeto URI necessário para que o código 202 seja retornado ao serializar com sucesso um json em um objeto em java.
+		return ResponseEntity.created(uri).body(user);
+		//Usar o 'created' 
+	}
 }
