639689f58dd7044a7f08d79a948a0b0bedbd997d
Andre Santana <andrelusantana@hotmail.com>
2021-02-24 20:33:56 -0300

    User update

--- src/main/java/br/com/andre/springbootcourse/services/UserService.java
+++ src/main/java/br/com/andre/springbootcourse/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void deleteById(Long id) {
 		repository.deleteById(id);
 	}
+	public User update(Long id, User user) {
+		User entity = repository.getOne(id);//Instacia obj monitorado, porém sem ir buscar no BD de forma imediata. 
+		updateData(entity, user);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
 }
