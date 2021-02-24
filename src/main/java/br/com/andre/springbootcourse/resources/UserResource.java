28be725fc8fc1fc49d80f7737cc5a33b1ac899da
Andre Santana <andrelusantana@hotmail.com>
2021-02-24 20:19:59 -0300

    User Delete

--- src/main/java/br/com/andre/springbootcourse/resources/UserResource.java
+++ src/main/java/br/com/andre/springbootcourse/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,5 +43,11 @@ public class UserResource {
 		// objeto URI necessário para que o código 202 seja retornado ao serializar com sucesso um json em um objeto em java.
 		return ResponseEntity.created(uri).body(user);
 		//Usar o 'created' 
+	}
+	
+	@DeleteMapping(value ="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
