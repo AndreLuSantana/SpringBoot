f0eee385b34f6f3643e18208724c73a5534f48b5
Andre Santana <andrelusantana@hotmail.com>
2021-03-01 06:24:50 -0300

    Exception handling - update

--- src/main/java/br/com/andre/springbootcourse/services/UserService.java
+++ src/main/java/br/com/andre/springbootcourse/services/UserService.java
@@ -3,6 +3,8 @@ package br.com.andre.springbootcourse.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,14 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User user) {
+		
+		try {
 		User entity = repository.getOne(id);//Instacia obj monitorado, porém sem ir buscar no BD de forma imediata. 
 		updateData(entity, user);
 		return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
